ee3fc2f42b272ee1906efa228e1b3501b6304917
Davide Depau <davide@depau.eu>
2017-07-29 23:59:33 +0200

    add license

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='nova6',
     version='2.0',
     packages=['nova6'],
-    url='',
+    url='https://github.com/Depaulicious/Nova6',
     license='GPLv2',
     author='Davide Depau',
     author_email='davide@depau.eu',
-    description='',
+    description='A torrent search engine based on qBittorrent\'s',
+    long_description=long_description,
     entry_points={
         "console_scripts": [
             "nova6 = nova6.nova6:main",
@@ -17,6 +23,25 @@ setup(
     },
     install_requires=[
         "requests",
-        "requests[socks]"
-    ]
+        "requests[socks]",
+        "PySocks!=1.5.7,>=1.5.6"
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='torrent search engine requests'
 )
